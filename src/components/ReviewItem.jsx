7c7ded0d2b00e848a2dd2ddd8aaf6b13ade06503
Austin Flatt <austindflatt@gmail.com>
2022-02-18 13:49:05 -0500

    fixed rating styling on card

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -13,10 +13,27 @@ export const Card = styled.div`
     position: relative;
 `;
 
+export const Rating = styled.div`
+    position: relative;
+    background: #f4f4f4;
+    width: 20px;
+    height: 20px;
+    padding: 10px;
+    text-align: center;
+    border-radius: 50%;
+    font-size: 19px;
+    border: 1px #eee solid;
+    transition: 0.3s;
+
+    position: absolute;
+    top: -10px;
+    left: -15px;
+`;
+
 function ReviewItem({ item }) {
   return (
     <Card>
-        <div className="rating-display">{item.rating}</div>
+        <Rating>{item.rating}</Rating>
         <div className="text-display">{item.text}</div>
     </Card>
   )
