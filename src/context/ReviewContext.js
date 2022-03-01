15a1eeb226300e27a076180ae2b9109893f4cff3
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-01 18:10:16 -0500

    brought in useEffect into context so data from mock back end JSON can load once page is loaded

--- src/context/ReviewContext.js
+++ src/context/ReviewContext.js
@@ -1,20 +1,25 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { v4 as uuidv4 } from 'uuid'
 
 const ReviewContext = createContext();
 
 export const ReviewProvider = ({ children }) => {
-    const [review, setReview] = useState([
-        {
-            id: 1,
-            text: 'This item is from context',
-            rating: 5
-        }
-    ]);
+    const [review, setReview] = useState([]);
     const [reviewEdit, setReviewEdit] = useState({
         item: {},
         edit: false,
     });
+
+    useEffect(() => {
+        fetchReview();
+    }, []);
+
+    // Fetch review data
+    const fetchReview = async () => {
+        const response = await fetch('http://localhost:3001/review?_sort=id&_order=desc');
+        const data = await response.json();
+        setReview(data);
+    }
 
     const addReview = (newReview) => {
         newReview.id = uuidv4();
